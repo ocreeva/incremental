185282d80dabe146c93602910e6556c20c777231
Brian Perrin <perrin@live.com>
2023-08-15 12:33:54 -0700

    Expose AccordionItemContext, AccordionState.

--- reach/accordion/index.ts
+++ reach/accordion/index.ts
@@ -1,4 +1,6 @@
 export { default as Accordion } from './Accordion';
 export { default as AccordionButton } from './AccordionButton';
 export { default as AccordionItem } from './AccordionItem';
+export { useAccordionItemContext } from './AccordionItemContext';
 export { default as AccordionPanel } from './AccordionPanel';
+export { default as AccordionState } from './AccordionState';
