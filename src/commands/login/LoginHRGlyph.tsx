735ee5bb624e75ce9ae0117f4d47cdfec6344e37
Brian Perrin <perrin@live.com>
2023-09-13 09:14:42 -0700

    Fix SVG attribute names for React.

--- src/commands/login/LoginHRGlyph.tsx
+++ src/commands/login/LoginHRGlyph.tsx
@@ -16,7 +16,7 @@ const LoginHRGlyph: React.FC
                         M -24 -24 v 48 h 48 v -48 z
                         M -11 12 a 2 2 0 0 1 -2 -2 v -2 a 8 7 0 0 1 16 0 v 2 a 2 2 0 0 1 -2 2" />
                 </clipPath>
-                <clipPath id={`${id}_meeple-clip`} clip-path={`url(#${id}_meeple-clip-body)`}>
+                <clipPath id={`${id}_meeple-clip`} clipPath={`url(#${id}_meeple-clip-body)`}>
                     <path d="
                         M -24 -24 v 48 h 48 v -48 z
                         M -5 2 a 5 5 0 0 1 0 -10 a 5 5 0 0 1 0 10" />
@@ -29,12 +29,12 @@ const LoginHRGlyph: React.FC
                 <path transform="rotate(120)" d="M 0 0 h 5 c 1 0 1 -2 0 -2 h -5" />
             </g>
             <path d="M -3 -1 h -8 a 1 1 0 0 0 0 2 h 8" />
-            <path fill-opacity="0.5" d="
+            <path fillOpacity="0.5" d="
                 M -4 -7 a 5 5 0 0 1 5 -5 h 6 a 5 5 0 0 1 5 5 v 14 a 5 5 0 0 1 -5 5 h -6 a 5 5 0 0 1 -5 -5 a 1 1 0 0 1 2 0
                 a 3 3 0 0 0 3 3 h 6 a 3 3 0 0 0 3 -3 v -14 a 3 3 0 0 0 -3 -3 h -6 a 3 3 0 0 0 -3 3 a 1 1 0 0 1 -2 0" />
 
             {/* HR components (meeples) */}
-            <g clip-path={`url(#${id}_frame-clip)`}>
+            <g clipPath={`url(#${id}_frame-clip)`}>
                 <g transform="translate(-1) scale(0.72) translate(12 -1)">
                     <g id={`${id}_meeple`}>
                         <path d="
@@ -43,12 +43,12 @@ const LoginHRGlyph: React.FC
                         <path d="
                             M -11 11 a 1 1 0 0 1 -1 -1 v -2 a 7 6 0 0 1 14 0 v 2 a 1 1 0 0 1 -1 1 z
                             M -0.5 9 a 0.5 0.5 0 0 0 0.5 -0.5 v -0.5 a 5 4 0 0 0 -10 0 v 0.5 a 0.5 0.5 0 0 0 0.5 0.5" />
-                        <g fill-opacity="0.25">
+                        <g fillOpacity="0.25">
                             <path d="M -5 -1 a 2 2 0 0 1 0 -4 a 2 2 0 0 1 0 4" />
                             <path d="M -0.5 9 a 0.5 0.5 0 0 0 0.5 -0.5 v -0.5 a 5 4 0 0 0 -10 0 v 0.5 a 0.5 0.5 0 0 0 0.5 0.5" />
                         </g>
                     </g>
-                    <g id={`${id}_meeple-offset`} clip-path={`url(#${id}_meeple-clip)`}>
+                    <g id={`${id}_meeple-offset`} clipPath={`url(#${id}_meeple-clip)`}>
                         <use href={`#${id}_meeple`} transform="translate(5 -2)" />
                     </g>
                 </g>
--- src/commands/login/LoginSecurityGlyph.tsx
+++ src/commands/login/LoginSecurityGlyph.tsx
@@ -18,17 +18,17 @@ const LoginSecurityGlyph: React.FC
                 <path transform="rotate(120)" d="M 0 0 h 5 c 1 0 1 -2 0 -2 h -5" />
             </g>
             <path d="M -3 -1 h -8 a 1 1 0 0 0 0 2 h 8" />
-            <path fill-opacity="0.5" d="
+            <path fillOpacity="0.5" d="
                 M -4 -7 a 5 5 0 0 1 5 -5 h 6 a 5 5 0 0 1 5 5 v 14 a 5 5 0 0 1 -5 5 h -6 a 5 5 0 0 1 -5 -5 a 1 1 0 0 1 2 0
                 a 3 3 0 0 0 3 3 h 6 a 3 3 0 0 0 3 -3 v -14 a 3 3 0 0 0 -3 -3 h -6 a 3 3 0 0 0 -3 3 a 1 1 0 0 1 -2 0" />
 
             {/* Security components (shield) */}
-            <g clip-path={`url(#${id}_frame-clip)`}>
+            <g clipPath={`url(#${id}_frame-clip)`}>
                 <g transform="translate(-1) scale(0.72) translate(10)">
                     <path d="
                         M 0 -12 l 9.24 3.83 v 10.21 a 8 8 0 0 1 -4 6.93 l -5.24 3.03 l -5.24 -3.03 a 8 8 0 0 1 -4 -6.93 v -10.21 z
                         m 0 2.31 l -7.24 2.86 v 8.87 a 6 6 0 0 0 3 5.2 l 4.24 2.45 l 4.24 -2.45 a 6 6 0 0 0 3 -5.2 v -8.87" />
-                    <g fill-opacity="0.25">
+                    <g fillOpacity="0.25">
                         <path d="M 0 0 v -9.69 l 7.24 2.86 v 6.83" />
                         <path d="M 0 0 v 9.69 l -4.24 -2.45 a 6 6 0 0 1 -3 -5.2 v -2.04" />
                     </g>
