09ac74d3c3312591da99b8fa80b40b72d6c85dae
Brian Perrin <perrin@live.com>
2023-09-14 11:14:07 -0700

    Remove importGlyph code.

--- src/assets/importGlyph.ts
+++
@@ -1,5 +0,0 @@
-const importGlyph = async (assetPath: string): Promise<React.FC<React.SVGProps<SVGSVGElement>>> => {
-    return (await import(`./glyphs/${assetPath}.svg`)).ReactComponent;
-};
-
-export default importGlyph;
