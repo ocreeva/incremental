dbd3608f7821cc1e60eebc525243caeca4186767
Brian Perrin <perrin@live.com>
2023-09-19 10:51:39 -0700

    Add requirements to Overclock level.

--- src/game/commands/overclock/OverclockModel.ts
+++ src/game/commands/overclock/OverclockModel.ts
@@ -1,7 +1,17 @@
-import { CommandId } from '@/constants';
+import { CommandId, type Host, Role } from '@/constants';
+import { ModelStatus } from '@/constants/worker';
 import CommandModel, { registerModel } from '@/game/commands/_/CommandModel';
 import type { EntityId } from '@/types';
 import type { IOperationModel } from '@/types/model';
+
+const rolesAllowedByLevel: Record<number, Role[]> = {
+    [0]: [Role.Root, Role.Admin, Role.User, Role.Guest, Role.Anon],
+    [1]: [Role.Root, Role.Admin, Role.User, Role.Guest],
+    [2]: [Role.Root, Role.Admin, Role.User],
+    [3]: [Role.Root, Role.Admin],
+    [4]: [Role.Root],
+    [5]: [],
+};
 
 class OverclockModel extends CommandModel {
     public static override readonly id: CommandId = CommandId.Overclock;
@@ -18,7 +28,20 @@ class OverclockModel extends CommandModel {
     public static override finalize(operationId: EntityId, time: number) {
         super.finalize(operationId, time);
 
-        this.level = Math.min(this.level + 1, 5);
+        // count the number of successful Overclock operations in distinct hosts
+        const hosts: Host[] = [];
+        const rolesAllowed = rolesAllowedByLevel[this.level];
+        this.game.operations.forEach((operation) => {
+            if (operation.commandId !== CommandId.Overclock) return;
+            if (operation.status !== ModelStatus.final) return;
+            if (!rolesAllowed.includes(operation.role)) return;
+            if (hosts.includes(operation.host)) return;
+            hosts.push(operation.host);
+        });
+
+        if (hosts.length <= this.level) return;
+
+        this.level++;
     }
 }
 
