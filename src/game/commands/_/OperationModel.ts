226af808ef0c9f59c1736f9a7d83250b68753d80
Brian Perrin <perrin@live.com>
2023-10-02 15:57:18 -0700

    Revert "Divorce Operation model from state, second pass."
    This reverts commit 4af5c7e1ffa5de705a389a09d048e0e5ce507972.

--- src/game/commands/_/OperationModel.ts
+++ src/game/commands/_/OperationModel.ts
@@ -6,10 +6,23 @@ import { DeltaValue } from '@/worker/client';
 import { ModelStatus } from '@/constants/worker';
 
 abstract class OperationModel implements IOperationModel {
+    private readonly state: OperationState;
     private remaining: number;
 
     protected constructor(parentRoutineId: EntityId, parentSubroutineId: EntityId) {
-        this.id = crypto.randomUUID();
+        this.state = {
+            id: crypto.randomUUID(),
+            commandId: this.derived.id,
+            parentRoutineId,
+            parentSubroutineId,
+            delay: 0,
+            duration: 42,
+            host: Host.Hub,
+            progress: 0,
+            role: Role.Anon,
+        };
+
+        this.id = this.state.id;
         this.parentRoutineId = parentRoutineId;
         this.parentSubroutineId = parentSubroutineId;
 
@@ -84,17 +97,7 @@ abstract class OperationModel implements IOperationModel {
         this.game = game;
         assert(this.game.operations.has(this.id), `Operation (${this.id}) missing from game context during initialization.`);
 
-        this.game.synchronization.addOperation({
-            id: this.id,
-            commandId: this.commandId,
-            parentRoutineId: this.parentRoutineId,
-            parentSubroutineId: this.parentSubroutineId,
-            delay: this.delay,
-            duration: this.duration,
-            host: this.host,
-            progress: this.progress,
-            role: this.role,
-        });
+        this.game.synchronization.addOperation(this.state);
 
         this.assertStatus(ModelStatus.loading);
         this.status = ModelStatus.pending;
